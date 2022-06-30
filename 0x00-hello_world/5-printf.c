0c08652ddaf77973f19e5cb12ad6cd7c60fe8e94
TolaniOkerinde <okerindeo@gmail.com>
2022-06-30 10:33:33 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's descriptioni'
+ * Return: Always 0 (Success)
+ */
+
+int main(){
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+return (0);
+}
+
