11bd155d177370ca46db10e50a7c12090c6f8a59
TolaniOkerinde <okerindeo@gmail.com>
2022-06-30 10:19:35 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's descriptioni'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char str[52];
+strcpy(str, "\"Programming is like building a multilingual puzzle");
+
+puts(str);
+return (0);
+}
